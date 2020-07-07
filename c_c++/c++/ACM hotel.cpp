83c15a38fde9dd0c975f53eb74fa9449d4855cbe
jhp336 <jhp336@naver.com>
2020-07-07 22:18:52 +0900

    acmhotel

--- c_c++/c++/ACM hotel.cpp
+++ c_c++/c++/ACM hotel.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main() {
     int t,h,w,n;
-    t=1;
-    h=6;
-    n=1;
-    
+    cin>>t;
     for(int i=0;i<t;i++){
-        
+        cin>>h>>w>>n;
         int a=n%h;
         int b=n/h+1;
-        
+        if(a==0){
+        a=h;
+        b--;
+        }
         cout<<100*a+b<<endl;
     }
     return 0;
