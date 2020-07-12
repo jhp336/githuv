1fc6425a90cd9523de48e9af2a1998b9ee203db4
jhp336 <jhp336@naver.com>
2020-07-12 18:31:33 +0900

    baekjoon

--- c_c++/c++/conference.cpp
+++ c_c++/c++/conference.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 int main() {
     int n,cnt=0;
-    n=9;
-    vector<pair <int,int>>a{
-        {8,8},{8,5},{4,3},{5,2},{7,2},{8,8},{10,1},{3,3},{10,10}};
-    
+    cin>>n;
+    vector<pair <int,int>>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i].second>>a[i].first;
+    }
     sort(a.begin(),a.end());
     int start=a[0].first;
     for (int i=0;i<n;i++){
