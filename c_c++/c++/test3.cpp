c4c069bbeb0d5df3e821bb0ad20a6584df033d25
jhp336 <jhp336@naver.com>
2020-07-18 19:57:18 +0900

    baekojoon

--- c_c++/c++/test3.cpp
+++ c_c++/c++/test3.cpp
@@ -16,35 +16,24 @@ int main() {
         for(int j=0;j<m;j++)
         vb[i][j]=s[j];
     }
-    if (m<3||n<3){
-        for (int i=0;i<n;i++){
-        for (int j=0;j<m;j++)
-            if (va[i][j]==vb[i][j])
-            cnt=0;
-            else {
-                cnt=-1;
-                break;
-            }
-            if(cnt==-1)
-            break;
-        }
-        }
-    else for (int i=0;i<n-2;i++){
+    for (int i=0;i<n-2;i++){
         for (int j=0;j<m-2;j++){
             if(va[i][j]!=vb[i][j]){
                 for(int k=i;k<i+3;k++){
-                for(int l=j;l<j+3;l++){
-                    if (va[k][l]=='0')
-                    va[k][l]='1';
-                    else va[k][l]='0';
-                }
+                    for(int l=j;l<j+3;l++){
+                        if (va[k][l]=='0')
+                        va[k][l]='1';
+                        else va[k][l]='0';
+                    }
                 }
                 cnt++;
             }
-            if (j==m-3){
-            if (va[i][j+1]!=vb[i][j+1]||va[i][j+2]!=vb[i][j+2])
-                cnt=-1;
-            }    
+        }
+    }   
+    for (int i=0;i<n;i++){
+        for (int j=0;j<m;j++){
+            if (va[i][j]!=vb[i][j])
+            cnt=-1;
         }
     }
     cout<<cnt;
