87157a7a49c531d5182b9849889a71596b9b341e
jhp336 <jhp336@naver.com>
2020-08-28 17:38:08 +0900

    loop

--- nodejs/main.js
+++ nodejs/main.js
@@ -5,38 +5,50 @@ const { REPL_MODE_SLOPPY } = require('repl');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url,true).query;
-    var title = queryData.id;
-    console.log(_url);
-    if(_url == '/'){
-      title = 'Welcome!';
+    var pathname = url.parse(_url,true).pathname;
+    if(pathname==='/'){
+        fs.readFile(`Data/${queryData.id}`,'utf8',function(err,para){
+            var title = queryData.id;
+            if(title===undefined){
+                var title = 'Welcome!';
+                var para = 'Welcome to Nodejs!';
+            }
+            fs.readdir('data',function(err,filelist){
+                var list=''
+                /*<li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+                */
+               for(var i=0;i<filelist.length;i++){
+                 var arr=[1,0,2];
+                    list=list+'<li><a href="?id='+filelist[arr[i]]+'">'+filelist[arr[i]]+'</a></li>'
+                }
+                var templat =`
+                    <!doctype html>
+                    <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ul>
+                        ${list}
+                        </ul>
+                        <h2>${title}</h2>
+                        <p>${para}</p>
+                    </body>
+                    </html>
+                `;
+                response.writeHead(200);
+                response.end(templat);
+            });
+        });
     }
-    if(_url == '/favicon.ico'){
+    else {
         response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile(`Data/${title}`,'utf8',function(err,para){
-    var templat =`
-      <!doctype html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${para}</p>
-        </body>
-      </html>
-    `;
-    response.end(templat);
-    })
+        response.end('Not Found');
+    }         
 });
+            
 app.listen(3000);
---
+++ nodejs/test.js
@@ -0,0 +1,7 @@
+var ar=process.argv;
+console.log(ar[2]);
+var a=ar[2];
+if(a===true){
+    console.log('a');
+}
+else console.log('b');
