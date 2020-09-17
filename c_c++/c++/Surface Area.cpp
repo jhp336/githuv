f9459604005a3752a89ed6fb627d01de9d5abcf3
jhp336 <jhp336@naver.com>
2020-09-17 22:33:30 +0900

    bj

---
+++ c_c++/c++/Surface Area.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+int main () {
+    int n,m;
+    cin>>n>>m;
+    long long ans=2*n*m;
+    int arr[n+2][m+2];
+    memset(arr,0,sizeof(arr));
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            if(arr[i][j]>arr[i+1][j])
+            ans=ans+arr[i][j]-arr[i+1][j];
+            if(arr[i][j]>arr[i-1][j])
+            ans=ans+arr[i][j]-arr[i-1][j];
+            if(arr[i][j]>arr[i][j-1])
+            ans=ans+arr[i][j]-arr[i][j-1];
+            if(arr[i][j]>arr[i][j+1])
+            ans=ans+arr[i][j]-arr[i][j+1];
+        }
+    }
+    cout<<ans;
+    return 0;
+}
