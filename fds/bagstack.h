ea90bebd3f893cb8f135f1c56f813c7e23bab2b7
jhp336 <jhp336@naver.com>
2020-09-29 23:10:49 +0900

    lab33

---
+++ fds/bagstack.h
@@ -0,0 +1,34 @@
+using namespace std;
+#include <iostream>
+
+class Bag
+{
+public:
+	Bag(int bagCapacity = 10);
+	virtual ~Bag();
+
+	virtual int Size() const;
+	virtual bool IsEmpty() const;
+	virtual int Element() const;
+
+	virtual void Push(const int);
+	virtual void Pop();
+
+	friend ostream& operator<<(ostream&, Bag &);
+
+protected: 
+	int *array;
+	int capacity;
+	int top;
+};
+
+class Stack: public Bag
+{
+public: 
+	Stack(int stackCapacity = 10);
+	~Stack();
+	int Top() const;
+	void Pop();
+};
+
+void ChangeSize1D(int*& a, const int oldsize, const int newSize);
